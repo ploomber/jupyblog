8a9f48aec282ddfda956b02b71c8afcadcdc02b6
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-02-24 15:30:36 -0500

    sample code working

--- parse.py
+++ parse.py
@@ -1,3 +1,9 @@
+import jupyter_client
+import mistune
+import yaml
+from jinja2 import Environment, FileSystemLoader, DebugUndefined, Template
+from pathlib import Path
+from functools import partial
 """
 pip install mistune==2.0.0a2 pyyaml jinja2
 
@@ -17,28 +23,88 @@ FIXME:
     but if that opt is on, it will merge the output, I need
     the output separate
 """
-from functools import partial
-from pathlib import Path
 
-from jinja2 import Environment, FileSystemLoader
-import yaml
-import mistune
+"""
+https://jupyter-client.readthedocs.io/en/stable/api/manager.html
+
+https://stackoverflow.com/questions/9977446/connecting-to-a-remote-ipython-instance
+https://stackoverflow.com/questions/33731744/executing-code-in-ipython-kernel-with-the-kernelclient-api
+
+can we use this instead of jupyter_client?
+https://ipython.readthedocs.io/en/stable/sphinxext.html
+"""
 
 
-def parse_metadata(md):
+class JupyterSession:
+
+    def __init__(self):
+        self.km = jupyter_client.KernelManager()
+        self.km.start_kernel()
+        self.kc = self.km.blocking_client()
+        self.out = []
+
+    def output_hook(self, msg):
+        # FIXME: this is not capturing all the output
+        # print(msg['msg_type'])
+        if msg['msg_type'] == 'stream':
+            self.out.append(msg['content']['text'])
+        elif msg['msg_type'] == 'execute_result':
+            self.out.append(msg['content']['data']['text/plain'])
+
+    def execute(self, code):
+        self.kc.execute_interactive(code,
+                                    output_hook=self.output_hook)
+        # verify new output is available, otherwise raise error
+        return self.out[-1]
+
+    def __del__(self):
+        self.km.shutdown_kernel()
+
+
+def parse_info(info):
+    if info is not None:
+        return {t.split('=')[0]: t.split('=')[1]
+                for t in info.split(' ')[1].split(',')}
+    else:
+        return {}
+
+
+class ASTExecutor:
+
+    def __init__(self):
+        self.session = JupyterSession()
+
+    def __call__(self, md_ast):
+        blocks = [e for e in md_ast if e['type'] == 'block_code']
+        # add parsed info
+        blocks = [{**block, **parse_info(block['info'])} for block in blocks]
+
+        for block in blocks:
+            if block.get('info'):
+                output = self.session.execute(block['text'])
+                print('In: ', block['text'])
+                print('>>> ', output)
+                block['output'] = output
+
+        return blocks
+
+    def __del__(self):
+        del self.session
+
+
+def parse_metadata(md_ast):
     """Parse markdown metadata
     """
-    markdown = mistune.create_markdown(renderer=mistune.AstRenderer())
-    ast = markdown(md)
     idx = 0
 
-    for e in ast:
+    for e in md_ast:
         if e['type'] == 'thematic_break':
             break
         else:
             idx += 1
 
-    return yaml.load(ast[idx+1]['children'][0]['text'], Loader=yaml.SafeLoader)
+    return yaml.load(md_ast[idx+1]['children'][0]['text'],
+                     Loader=yaml.SafeLoader)
 
 
 def expand(path, root_path):
@@ -55,12 +121,74 @@ class MarkdownRenderer:
 
     def __init__(self, path_to_mds):
         self.path = path_to_mds
-        self.env = Environment(loader=FileSystemLoader(path_to_mds))
+        self.env = Environment(loader=FileSystemLoader(path_to_mds),
+                               undefined=DebugUndefined)
+        self.parser = mistune.create_markdown(renderer=mistune.AstRenderer())
 
     def render(self, name):
-        metadata = parse_metadata(Path(self.path, name).read_text())
+        md_raw = Path(self.path, name).read_text()
+        md_ast = self.parser(md_raw)
+        metadata = parse_metadata(md_ast)
         self.env.globals['expand'] = partial(expand,
                                              root_path=metadata['root_path'])
+
+        # first render - expand
         content = self.env.get_template(name).render()
+
+        print(content)
+
         del self.env.globals['expand']
-        return content
+
+        # parse again to get expanded code
+        md_ast = self.parser(content)
+
+        # second render, add output
+        executor = ASTExecutor()
+        blocks = executor(md_ast)
+
+        md_out = Template(content).render(**{block['id']: block['output']
+                                             for block
+                                             in blocks if block.get('id')})
+
+        for block in blocks:
+            if block.get('hide'):
+                to_replace = "```{}\n{}```".format(block['info'], block['text'])
+                md_out = md_out.replace(to_replace, '')
+
+        for block in blocks:
+            if block.get('info'):
+                md_out = md_out.replace(block['info'], block['info'].split(' ')[0])
+
+        return md_out
+
+
+md_raw = """
+```python hide=true
+x = 10
+```
+
+```python id=my_sum
+x + 1
+```
+
+```
+{{my_sum}}
+```
+
+# header
+
+```python id=another_sum
+2 + 2
+```
+
+```
+{{another_sum}}
+```
+
+"""
+
+
+mdr = MarkdownRenderer('.')
+out = mdr.render('sample.md')
+print(out)
+Path('out.md').write_text(out)
--- sample.md
+++ sample.md
@@ -3,15 +3,20 @@ key: 1
 root_path: /Users/Edu/dev/posts/self-documented-data
 ---
 
-```python
-import os
-os.chdir('/Users/Edu/dev/posts/self-documented-data')
-```
-
 # header
 
-```python
+```python hide=true
+import os
+os.chdir('/Users/Edu/dev/posts/self-documented-data')
+print(42)
+```
+
+Sample code:
+
+```python id=main
 {{ expand('main.py') }}
 ```
 
+```
 {{main}}
+```
